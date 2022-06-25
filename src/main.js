240466c4e24ecab6d429dd67b5620acb830865ba
Belle <belle_xu@apple.com>
2022-06-25 23:39:02 +0800

    项目初始化 - 引入vant组件库

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//加载 vant 核心组件库
+import Vant from 'vant'
+//加载 vant 全局样式
+import 'vant/lib/index.css'
 //加载全局样式
 import "./style/index.less"
+
+//注册使用vant组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
